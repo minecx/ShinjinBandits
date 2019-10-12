8875344bdef27ff4afe091779b407a5483d04f49
Bob Zhang <chz026@ucsd.edu>
2019-10-11 20:59:09 -0700

    Initial commit for iOS version

---
+++ iOS/CameraTracker/CameraTracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CameraTracker
+//
+//  Created by Chongbin (Bob) Zhang on 2019/10/11.
+//  Copyright © 2019 Bob. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
