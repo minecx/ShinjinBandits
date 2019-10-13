6a5faca6697947df1ede2549243eb003d62b2fff
Bob Zhang <chz026@ucsd.edu>
2019-10-13 13:04:20 -0700

    Simple UI, places for functionalities, sign up page

--- CameraTracker/CameraTracker/ViewController.swift
+++ CameraTracker/CameraTracker/ViewController.swift
@@ -9,7 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var LoginUsername: UITextField!
+    @IBOutlet weak var LoginPassword: UITextField!
+    @IBAction func log_in(_ sender: Any) {
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
