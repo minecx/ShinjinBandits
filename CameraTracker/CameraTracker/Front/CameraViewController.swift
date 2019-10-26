96b73787e009d3d4b3fbc5cf1e80dcd0dd741730
Bob Zhang <chz026@ucsd.edu>
2019-10-26 16:45:55 -0700

    UI for camera view & view controller file, need functionality

---
+++ CameraTracker/CameraTracker/Front/CameraViewController.swift
@@ -0,0 +1,9 @@
+//
+//  CameraViewController.swift
+//  CameraTracker
+//
+//  Created by Chongbin (Bob) Zhang on 2019/10/26.
+//  Copyright © 2019 Bob. All rights reserved.
+//
+
+import Foundation
--- CameraTracker/CameraTracker/Front/LoginViewController.swift
+++ CameraTracker/CameraTracker/Front/LoginViewController.swift
@@ -14,19 +14,33 @@ class ViewController: UIViewController {
         pingHost(IPAddress.text!)
     }
     
-    func pingHost(_ IPAddress: String) {
-        let _test: String = "https://api.datamuse.com/words?ml=antic+city"
-        let url = URL(string: IPAddress)
+    func showAlert() {
+        let alertController = UIAlertController(title: "iOScreator", message:
+            "Hello, world!", preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "Dismiss", style: .default))
 
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+    func pingHost(_ IPAddress: String) {
+        let url = URL(string: IPAddress)
+        let defaultURL = URL(string: "1.1.1.1")
+        print("url: \(String(describing: url ?? defaultURL))")
+        /*
+        let test: String = "https://api.datamuse.com/words?ml=antic+city"
+        let url = URL(string: test)
+        */
         let task = URLSession.shared.dataTask(with: url!) { _, response, _ in
             if let httpResponse = response as? HTTPURLResponse {
                 print("Response code: \(httpResponse.statusCode)")
+                // return true
             }
             else {
-                print("Connected failed, httpResponse: \(String(describing: response))")
+                // return false
             }
         }
         task.resume()
+        // return true
     }
     
     override func viewDidLoad() {
