f3ef40923ae7a34e9e58e2334d2d7c74a8ece82e
Bob Zhang <chz026@ucsd.edu>
2019-11-27 18:48:15 -0800

    applied semantic UI, made a preliminary sign in page

--- camera_tracker_web/src/App.js
+++ camera_tracker_web/src/App.js
@@ -1,26 +1,42 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'semantic-ui-css/semantic.min.css';
+import {
+  Button,
+  Form,
+  Grid,
+  Header,
+  Message,
+  Segment,
+} from 'semantic-ui-react';
+import Login from './login.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  renderDefault() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+
+  render() {
+    return (
+      <Login />
+    )
+  }
 }
-
-export default App;
---
+++ camera_tracker_web/src/login.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import './App.css';
+import 'semantic-ui-css/semantic.min.css';
+import {
+    Button,
+    Form,
+    Grid,
+    Header,
+    Message,
+    Segment,
+} from 'semantic-ui-react';
+
+export default class Login extends React.Component {
+  render() {
+    return (
+      <Grid centered columns={2} style={{marginTop: '5%'}}>
+        <Grid.Column>
+        <Header as="h2" textAlign="center">
+            Login to your Camera
+        </Header>
+        <Segment>
+            <Form size="large">
+            <Form.Input
+                fluid
+                icon="user"
+                iconPosition="left"
+                placeholder="IP address"
+            />
+            <Form.Input
+                fluid
+                icon="lock"
+                iconPosition="left"
+                placeholder="Password (optional)"
+                type="password"
+            />
+            <Button color="blue" fluid size="large">
+                Login
+            </Button>
+            </Form>
+        </Segment>
+        <Message>
+            Not registered yet? <a href="//linkedin.com">Sign Up</a>
+        </Message>
+        </Grid.Column>
+      </Grid>
+    )
+  }
+}
