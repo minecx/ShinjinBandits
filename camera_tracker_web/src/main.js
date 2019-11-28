8836b4a631df0efa0544c73a36f61f501d22d646
Bob Zhang <chz026@ucsd.edu>
2019-11-27 23:44:50 -0800

    updated styles in main video display page

--- camera_tracker_web/src/main.js
+++ camera_tracker_web/src/main.js
@@ -1,18 +1,42 @@
 import React from 'react';
 import './App.css';
+import styled from 'styled-components';
 import 'semantic-ui-css/semantic.min.css';
-import { Embed } from 'semantic-ui-react';
+import { Embed, Button } from 'semantic-ui-react';
 
 export default class Main extends React.Component {
   render() {
     return (
-      <div style={{width: '75%', height: '90%', marginTop: '5%', marginLeft: '12.5%'}}>
+      <Page>
         <Embed 
           id='O6Xo21L0ybE'
           placeholder='/images/image-16by9.png'
           source='youtube'
         />
-      </div>
+        <Buttons>
+          <Button>Up</Button>
+          <Button>Down</Button>
+          <Button>Left</Button>
+          <Button>Right</Button>
+          <Button>Refresh</Button>
+        </Buttons>
+        {/* onChange={window.location.reload(false)} : code snippet for refreshing page */}
+      </Page>
     )
   }
 }
+
+const Page = styled.div `
+  width: 75%;
+  height: 90%;
+  margin-top: 2.5%;
+  margin-left: 12.5%;
+`
+
+const Buttons = styled.div `
+  height: 90%;
+  margin-top: 2.5%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+`
